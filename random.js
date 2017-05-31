29f7fcad0310324ede709e34630bfb4eec26740d
oshoferyuk <oshoferyuk@gmail.com>
2017-05-31 23:42:01 +0300

    merge

--- random.js
+++ random.js
@@ -1,10 +1,10 @@
 000
 function getRandomNumber(min, max){
 	
-	return 99 * Math.floor(Math.random() * (max - min + 1)) + min/2;
+	//return 99 * Math.floor(Math.random() * (max - min + 1)) + min/2;
 
 }
 
 function getRandomNumber2(op){
-	op => () => ({a:23});
+	op => () => ({a:23}) * 99;
 }
