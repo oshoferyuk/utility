729acbac6e6b79a617a409de016d1033561398e7
oshoferyuk <oshoferyuk@gmail.com>
2017-05-31 16:18:37 +0300

    bug123 adding 99

--- random.js
+++ random.js
@@ -1,5 +1,5 @@
 function getRandomNumber(min, max){
-	return Math.floor(Math.random() * (max - min + 1)) + min;
+	return 99 * Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
 function getRandomNumber2(op){
